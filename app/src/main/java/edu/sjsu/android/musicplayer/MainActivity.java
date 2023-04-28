e0825e8488368264f2c00e03b194adcdfba2bdde
Catherine Pham <catherine.pham@sjsu.edu>
2023-04-27 17:37:34 -0700

    First stage of Spotify feature

--- app/src/main/java/edu/sjsu/android/musicplayer/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/musicplayer/MainActivity.java
@@ -10,16 +10,27 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.Manifest;
 import android.widget.Toast;
 
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
 import java.io.File;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String CLIENT_ID = "2bbb51fe700f4a40804482e9ed90fc05";
+    private static final String REDIRECT_URI = "http://edu.sjsu.android.musicplayer/callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
     RecyclerView list;
     TextView    noSongs;
     ArrayList<Audio> songsList = new ArrayList<>();
@@ -77,5 +88,58 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "Access to external storage is required to find songs", Toast.LENGTH_SHORT).show();
         else
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 123);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Set the connection parameters
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+        // Connect to app remote
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+                        // Start interacting with app remote
+                        connected();
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MainActivity", throwable.getMessage(), throwable);
+                        // Connection failed, handle errors here
+                    }
+                });
+    }
+
+    /**
+     * Behavior of Spotify app remote once connected
+     */
+    private void connected() {
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Toast.makeText(this, track.name + " by " + track.artist.name, Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Disconnect app remote
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
 }
